fb4a4541ae735d81a9aba44e84305b5ff773bd10
Sem Zhivotov <semzhivotov@gmail.com>
2023-06-30 13:25:32 +0300

    create DAO layer

--- src/main/java/com/forum/dao/RoleDao.java
+++ src/main/java/com/forum/dao/RoleDao.java
@@ -1,6 +1,7 @@
 package com.forum.dao;
 
+import com.forum.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleDao extends JpaRepository<RoleDao, Long> {
+public interface RoleDao extends JpaRepository<Role, Long> {
 }
