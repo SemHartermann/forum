af76466269d7cbc5b5a5bf06edc0c6ca71027bef
Sem Zhivotov <semzhivotov@gmail.com>
2023-07-03 22:10:21 +0300

    add security config

---
+++ src/main/java/com/forum/security/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.forum.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from user where user_id=?");
+
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, name from user_role where user_id=?");
+
+        return jdbcUserDetailsManager;
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+                        configurer
+
+                                .anyRequest().authenticated()
+                )
+                .exceptionHandling(configurer ->
+                        configurer
+                                .accessDeniedPage("/access-denied"))
+                .formLogin(form ->
+                        form
+                                .loginPage("/showMyLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll);
+
+
+        return http.build();
+    }
+}
